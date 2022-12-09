5994f903bb315e3dbb7cb02a3ded01ddab1f126c
MajGucek <maj.gucek@gmail.com>
2022-12-09 21:30:12 +0100

    Holy shit bre 🦀

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![allow(dead_code, unused)]
 #![allow(non_snake_case)]
+
+use std::f32::consts::TAU;
 use bevy::prelude::*;
 use bevy::input::mouse::{MouseWheel,MouseMotion};
 use bevy::render::camera::Projection;
 use bevy::pbr::wireframe::{Wireframe, WireframePlugin};
+use bevy::prelude::shape::Cube;
 use bevy::time::FixedTimestep;
 use bevy::transform::transform_propagate_system;
 use rand::*;
@@ -13,6 +16,32 @@ use rand::*;
 struct Camera {
     speed: f32,
 }
+
+#[derive(Component)]
+struct Blocks {
+    speed: f32,
+}
+
+#[derive(Component)]
+pub struct Movable {
+    pub auto_despawn: bool,
+}
+
+#[derive(Resource)]
+struct Cubes {
+    cube_count: u32,
+}
+
+impl Cubes {
+    fn increase(&mut self) {
+        self.cube_count += 1;
+    }
+
+    fn decrease(&mut self) {
+        self.cube_count -= 1;
+    }
+}
+
 
 
 fn main() {
@@ -30,85 +59,62 @@ fn main() {
             close_when_requested: true,
         }))
         .add_startup_system(setup)
+        .insert_resource(Cubes {cube_count: 0})
         .add_system(move_camera)
-        .add_system(spawn_cubes)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(0.05))
+                .with_system(spawn_cubes),
+        )
         .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.7)))
         .add_plugin(WireframePlugin)
+        .add_system(move_cubes)
+        .add_system(despawn_cubes)
         .run();
 }
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
-    // camera
-    let translation = Vec3::new(0.0, 50.0, 0.0000000000001);
-    let radius = translation.length();
-    commands.spawn((Camera3dBundle {
 
-        transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    },
-        Camera { speed: 10.0 }
-    ));
-    // camera hitbox
-    commands.spawn((PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube {
-            size: 5.0,
-        })),
-        transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y),
-        visibility: Visibility {
-            is_visible: false,
-        },
-        ..default()
-    },
-        Camera { speed: 10.0 }
-    ));
+fn despawn_cubes(mut commands: Commands, mut query: Query<(Entity, &mut Transform, &Movable)>, mut cubes: ResMut<Cubes>) {
+    for (entity, mut transform, movable) in query.iter_mut() {
+        if movable.auto_despawn == true && transform.translation.y > 25.0 {
+            commands.entity(entity).despawn();
+            cubes.decrease();
+        }
+    }
+}
 
+fn move_cubes(mut blocks: Query<(&mut Transform, &Blocks)>, timer: Res<Time>) {
+    for (mut transform, block) in &mut blocks {
+        let dir_y = transform.local_y();
+        transform.translation += dir_y * timer.delta_seconds() * block.speed;
+    }
+}
 
+fn spawn_cubes(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>, mut cubes: ResMut<Cubes>, query: Query<Entity, With<Blocks>>) {
+        // generate cubes in range [-25, 25]
+        let mut rng = thread_rng();
+        let mut x_range: f32 = rng.gen_range(-25.0..=25.0);
+        let mut y_range: f32 = rng.gen_range(-15.0..=-5.0);
+        let mut z_range: f32 = rng.gen_range(-25.0..=25.0);
+        let mut coords: Vec3 = Vec3::new(x_range, y_range, z_range);
 
-
-    // light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1000.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_translation(Vec3::new(0.0, 25.0, 0.0)),
-        ..default()
-    });
-
-
-    // normal cube
-    commands.spawn((PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Box {
-            min_x: -2.0,
-            max_x: 2.0,
-            min_y: -2.0,
-            max_y: 2.0,
-            min_z: -2.0,
-            max_z: 2.0,
-        })),
-        material: materials.add(Color::RED.into()),
-        transform: Transform::from_translation(Vec3::new(5.0, 0.0, 0.0)),
-        ..default()
-    }));
-
-    // center cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube {
-            size: 0.5,
-        })),
-        material: materials.add(Color::GREEN.into()),
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
-        ..default()
-    });
+        if cubes.cube_count < 50 {
+                commands.spawn((PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Cube {
+                        size: 5.0,
+                    })),
+                    material: materials.add(Color::MIDNIGHT_BLUE.into()),
+                    transform: Transform::from_translation(coords),
+                    ..default()
+                },
+                                Blocks { speed: 10.0 },
+                                Movable { auto_despawn: true },
+                ));
+                cubes.increase();
+        }
 }
 
 
 
-fn spawn_cubes(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
-
-
-    let mut rng = thread_rng();
-}
 
 fn move_camera(mut cameras: Query<(&mut Transform, &Camera)>, keys: Res<Input<KeyCode>>, timer: Res<Time>) {
     for (mut transform, camera) in &mut cameras {
@@ -124,18 +130,55 @@ fn move_camera(mut cameras: Query<(&mut Transform, &Camera)>, keys: Res<Input<Ke
             println!("{}", transform.translation.x);
 
         }
-        if keys.pressed(KeyCode::W) && transform.translation.y < 25.0 {
+        if keys.pressed(KeyCode::S) && transform.translation.z < 25.0 {
+            transform.translation -= dir_y * timer.delta_seconds() * camera.speed;
+            println!("{}", transform.translation.z);
+        }
+        if keys.pressed(KeyCode::W) && transform.translation.z > -25.0 {
             transform.translation += dir_y * timer.delta_seconds() * camera.speed;
             println!("{}", transform.translation.z);
         }
-        if keys.pressed(KeyCode::S) && transform.translation.y > -25.0 {
-            transform.translation += -dir_y * timer.delta_seconds() * camera.speed;
-            println!("{}", transform.translation.z);
-        }
-
-
-
-
     }
+}
+
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    // camera
+    let translation = Vec3::new(0.0, 50.0, 0.0000000000001);
+    let radius = translation.length();
+    commands.spawn((Camera3dBundle {
+
+        transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    },
+                    Camera { speed: 10.0 }
+    ));
+    // camera hitbox
+    commands.spawn((PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube {
+            size: 5.0,
+        })),
+        transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y),
+        visibility: Visibility {
+            is_visible: false,
+        },
+        ..default()
+    },
+                    Camera { speed: 10.0 }
+    ));
+
+
+
+
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(0.0, 25.0, 0.0)),
+
+        ..default()
+    });
 }
 
