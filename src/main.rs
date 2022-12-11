3ef50856a76b2942936553ce550ba66193d0ec27
MajGucek <maj.gucek@gmail.com>
2022-12-11 15:21:44 +0100

    Implement full Death scene

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,9 @@
 #![allow(non_snake_case)]
 #![feature(core_panic)]
 
-extern crate core;
-
-use core::panicking::panic;
-use std::f32::consts::TAU;
 use bevy::prelude::*;
-use bevy::input::mouse::{MouseWheel,MouseMotion};
-use bevy::render::camera::Projection;
-use bevy::pbr::wireframe::{Wireframe, WireframePlugin};
 use bevy::prelude::shape::Cube;
 use bevy::time::FixedTimestep;
-use bevy::transform::transform_propagate_system;
-use bevy::ui::FocusPolicy::Block;
 use rand::*;
 
 
@@ -35,8 +26,8 @@ pub struct Movable {
 #[derive(Resource)]
 struct Cubes {
     cube_count: u32,
+    intensity: u32,
 }
-
 impl Cubes {
     fn increase(&mut self) {
         self.cube_count += 1;
@@ -45,6 +36,35 @@ impl Cubes {
     fn decrease(&mut self) {
         self.cube_count -= 1;
     }
+
+
+    fn increase_intensity(&mut self) {
+        self.intensity += 10;
+    }
+}
+
+#[derive(Resource)]
+struct Death {
+    is_game_over: bool,
+}
+
+impl Death {
+    pub fn die(&mut self) {
+        self.is_game_over = true;
+    }
+}
+
+
+
+#[derive(Resource)]
+struct Score {
+    score: u64,
+}
+
+
+#[derive(Resource)]
+struct Counter {
+    count: u32,
 }
 
 
@@ -64,7 +84,10 @@ fn main() {
             close_when_requested: true,
         }))
         .add_startup_system(setup)
-        .insert_resource(Cubes {cube_count: 0})
+        .insert_resource(Counter { count: 0 })
+        .insert_resource(Score { score: 0 })
+        .insert_resource(Cubes { cube_count: 0, intensity: 100 })
+        .insert_resource(Death { is_game_over: false })
         .add_system(move_camera)
         .add_system_set(
             SystemSet::new()
@@ -77,33 +100,15 @@ fn main() {
                 .with_system(increase_speed),
         )
         .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.7)))
-        .add_plugin(WireframePlugin)
         .add_system(move_cubes)
         .add_system(despawn_cubes)
         .add_system(player_collision_checker)
+        .add_system(death_scene)
+        .add_system(increase_intensity)
         .run();
 }
 
 
-
-
-
-
-fn despawn_cubes(mut commands: Commands, mut query: Query<(Entity, &mut Transform, &Movable)>, mut cubes: ResMut<Cubes>) {
-    for (entity, mut transform, movable) in query.iter_mut() {
-        if movable.auto_despawn == true && transform.translation.y > 55.0 {
-            commands.entity(entity).despawn();
-            cubes.decrease();
-        }
-    }
-}
-
-fn move_cubes(mut blocks: Query<(&mut Transform, &mut Blocks)>, timer: Res<Time>) {
-    for (mut transform, block) in &mut blocks {
-        let dir_y = transform.local_y();
-        transform.translation += dir_y * timer.delta_seconds() * block.speed;
-    }
-}
 
 fn increase_speed(mut blocks: Query<(&mut Blocks)>) {
     for (mut block) in &mut blocks {
@@ -112,7 +117,49 @@ fn increase_speed(mut blocks: Query<(&mut Blocks)>) {
 }
 
 
-fn spawn_cubes(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>, mut cubes: ResMut<Cubes>, query: Query<Entity, With<Blocks>>) {
+fn increase_intensity(
+    mut cubes: ResMut<Cubes>
+) {
+    cubes.increase_intensity();
+}
+
+fn despawn_cubes(
+    mut commands: Commands,
+    mut query: Query<(Entity, &mut Transform, &Movable)>,
+    mut cubes: ResMut<Cubes>
+) {
+    for (entity, mut transform, movable) in query.iter_mut() {
+        if movable.auto_despawn == true && transform.translation.y > 55.0 {
+            commands.entity(entity).despawn();
+            cubes.decrease();
+        }
+    }
+}
+
+fn move_cubes(
+    mut blocks: Query<(&mut Transform, &mut Blocks)>,
+    timer: Res<Time>,
+    mut score: ResMut<Score>,
+    death: Res<Death>,
+) {
+    if !death.is_game_over {
+        score.score += 1;
+    }
+
+    for (mut transform, block) in &mut blocks {
+        let dir_y = transform.local_y();
+        transform.translation += dir_y * timer.delta_seconds() * block.speed;
+    }
+}
+
+fn spawn_cubes(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut cubes: ResMut<Cubes>, query: Query<Entity, With<Blocks>>,
+    death: Res<Death>,
+) {
+    if death.is_game_over == false {
         // generate cubes in range [-25, 25]
         let mut rng = thread_rng();
         let mut x_range: i32 = rng.gen_range(-50..=50); //   <-    ->
@@ -123,63 +170,69 @@ fn spawn_cubes(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut mat
         let mut coords: Vec3 = Vec3::new(x_range as f32, y_range as f32, z_range as f32);
 
 
-        if cubes.cube_count < 100 {
-                commands.spawn((PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::Box {
-                        min_x: -2.5, // left - right
-                        max_x: 2.5,
-                        min_y: -2.5, // lenght 3D
-                        max_y: 2.5,
-                        min_z: -2.5, // up down
-                        max_z: 2.5,
-                    })),
-                    material: materials.add(Color::MIDNIGHT_BLUE.into()),
-                    transform: Transform::from_translation(coords),
-                    ..default()
-                },
-                                Blocks { speed: 10.0 },
-                                Movable { auto_despawn: true },
-                ));
-                cubes.increase();
+        if cubes.cube_count < cubes.intensity {
+            commands.spawn((PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Box {
+                    min_x: -2.5, // left - right
+                    max_x: 2.5,
+                    min_y: -2.5, // lenght 3D
+                    max_y: 2.5,
+                    min_z: -2.5, // up down
+                    max_z: 2.5,
+                })),
+                material: materials.add(Color::MIDNIGHT_BLUE.into()),
+                transform: Transform::from_translation(coords),
+                ..default()
+            },
+                            Blocks { speed: 10.0 },
+                            Movable { auto_despawn: true },
+            ));
+            cubes.increase();
         }
+    }
 }
 
-
-
-
-fn move_camera(mut cameras: Query<(&mut Transform, &Camera)>, keys: Res<Input<KeyCode>>, timer: Res<Time>) {
+fn move_camera(
+    mut cameras: Query<(&mut Transform, &Camera)>,
+    keys: Res<Input<KeyCode>>, timer: Res<Time>
+) {
     for (mut transform, camera) in &mut cameras {
         let dir_x = transform.local_x();
         let dir_y = transform.local_y();
         if keys.pressed(KeyCode::A) && transform.translation.x > -40.0 {
             transform.translation += -dir_x * timer.delta_seconds() * camera.speed;
-            println!("Moved Left");
+            // println!("Moved Left");
 
         }
         if keys.pressed(KeyCode::D) && transform.translation.x < 40.0 {
             transform.translation += dir_x * timer.delta_seconds() * camera.speed;
-            println!("Moved Right");
+            // println!("Moved Right");
 
         }
         if keys.pressed(KeyCode::S) && transform.translation.z < 40.0 {
             transform.translation -= dir_y * timer.delta_seconds() * camera.speed;
-            println!("Moved Down");
+            // println!("Moved Down");
         }
         if keys.pressed(KeyCode::W) && transform.translation.z > -40.0 {
             transform.translation += dir_y * timer.delta_seconds() * camera.speed;
-            println!("Moved Up");
+            // println!("Moved Up");
         }
     }
 }
 
-fn player_collision_checker(mut commands: Commands, mut blocks: Query<(&Transform, &Blocks)>, mut cameras: Query<(&Transform, &Camera)>) {
+fn player_collision_checker(
+    mut commands: Commands,
+    mut blocks: Query<(&Transform, &Blocks)>,
+    mut cameras: Query<(&Transform, &Camera)>,
+    mut death: ResMut<Death>
+) {
     for (mut BlockTransform, block) in blocks.iter_mut() {
         for (mut CameraTransform, camera) in &mut cameras {
 
             if (CameraTransform.translation.x > BlockTransform.translation.x - 2.5 && CameraTransform.translation.x < BlockTransform.translation.x + 2.5) &&
                 (CameraTransform.translation.y > BlockTransform.translation.y - 2.5 && CameraTransform.translation.y < BlockTransform.translation.y + 2.5) &&
                 (CameraTransform.translation.z > BlockTransform.translation.z - 2.5 && CameraTransform.translation.z < BlockTransform.translation.z + 2.5) {
-                todo!("Implement Death");
+                death.die();
             }
 
 
@@ -190,8 +243,30 @@ fn player_collision_checker(mut commands: Commands, mut blocks: Query<(&Transfor
     }
 }
 
+fn death_scene(
+    mut commands: Commands,
+    death: Res<Death>,
+    entities: Query<Entity, Without<Camera>>,
+    score: Res<Score>,
+    mut counter: ResMut<Counter>,
+) {
+    if death.is_game_over == true {
+        if counter.count == 0 {
+            for entity in &entities {
+                commands.entity(entity).despawn_recursive();
+            }
+            println!("Score: {}", score.score);
+        }
+        counter.count += 1;
+    }
+}
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>
+) {
     // camera
     let translation = Vec3::new(0.0, 50.0, 0.0000000000001);
     let radius = translation.length();
